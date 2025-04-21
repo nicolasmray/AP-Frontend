068f4c70579521238a595527cca8ba55d2644881
nicolas.mray <nicolas.mray@gmail.com>
2025-04-21 23:52:43 +0200

    login1

--- ExpenseAdminSystemAngular/src/app/app.routes.ts
+++ ExpenseAdminSystemAngular/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "user", component: UserComponent },
+    { path: "user-list", component: UserListComponent },
+    { path: "login", component: LoginComponent }
+
+
+
+];
--- ExpenseAdminSystemAngular/src/app/user-list/user-list.component.ts
+++ ExpenseAdminSystemAngular/src/app/user-list/user-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { User } from '../model/user';
 import { UserService } from '../services/user.service';
 import { UserComponent } from '../user/user.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
@@ -13,8 +14,12 @@ import { UserComponent } from '../user/user.component';
 export class UserListComponent {
   users: User[] = [];
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
   ngOnInit(): void {
+    if(this.userService.authHeader == null) {
+      this.router.navigate(["login"]);
+    }
+
     this.userService.getUsers().subscribe(listOfUsers=>{
       this.users = listOfUsers
     })
