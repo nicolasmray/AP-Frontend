068649aa54291fbabd35ace3ca626a84b32352a3
nicolas.mray <nicolas.mray@gmail.com>
2025-05-02 10:35:30 +0200

    account1

--- ExpenseAdminSystemAngular/src/app/account-edit/account-edit.component.ts
+++ ExpenseAdminSystemAngular/src/app/account-edit/account-edit.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
+import { User } from '../model/user';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
+import { UserComponent } from '../user/user.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-account-edit',
   standalone: true,
-  imports: [],
+  imports: [UserComponent, FormsModule],
   templateUrl: './account-edit.component.html',
   styleUrl: './account-edit.component.css'
 })
 export class AccountEditComponent {
+user?: User;
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit(): void {
+    if(this.userService.authHeader == null) {
+      this.router.navigate(["login"]);
+    }
+
+    const userIdString = localStorage.getItem('id');
+ 
+
+    if (userIdString) {
+      const userId = Number(userIdString);
+      this.userService.getUser(userId).subscribe({
+        next: (data) => {
+          this.user = data;
+        },
+        error: (err) => {
+          console.error('Failed to fetch user:', err);
+        }
+      });
+    } else {
+      console.warn('No user ID found in localStorage');
+    }
+  }
+
+  confirmPassword: string = '';
+
+  modifyInfo(): void {
+
+    
+  }
+
+
+  saveChanges(): void {
+
+    this.router.navigate(['account'])
+  }
+
+
+
 
 }
--- ExpenseAdminSystemAngular/src/app/app.routes.ts
+++ ExpenseAdminSystemAngular/src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ExpenseListComponent } from './expense-list/expense-list.component';
 import { AccountComponent } from './account/account.component';
+import { AccountEditComponent } from './account-edit/account-edit.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: "home", component: HomeComponent },
     { path: "account", component: AccountComponent },
+    { path: "account-edit", component: AccountEditComponent },
     { path: "expenses", component: ExpenseListComponent },
     { path: "user", component: UserComponent },
     { path: "user-list", component: UserListComponent },
